b1c9b0d8384c551508d1c46f1cee7ec256f1b9cf
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-03-20 00:02:10 +0700

    Update _buttonResponse.js

--- plugins/_buttonResponse.js
+++ plugins/_buttonResponse.js
@@ -1,4 +1,4 @@
-const { proto, generateWAMessage,  areJidsSameUser } = require('@adiwajshing/baileys')
+const { proto, generateWAMessage,  areJidsSameUser } = require('@adiwajshing/baileys-md')
 //const util = require('util')
 
 module.exports = {
